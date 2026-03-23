8305dd8e545c1b5af4035704a1231d90d9f6e2fa
Allanella <allanbaliddawa@gmail.com>
2026-03-23 15:24:16 +0300

    chore: strict prisma 7 compliance - moved url to config

--- Joblify-backend/lib/prisma.mjs
+++ Joblify-backend/lib/prisma.mjs
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-// In Prisma 7, we pass the datasource URL directly into the constructor
 const prisma = new PrismaClient({
   datasource: {
     url: process.env.DATABASE_URL
